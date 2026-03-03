239bb0b3d8be12595e0c36f828b604ca55d1ecc8
Marcos Conde <marcos14conde@hotmail.com>
2026-03-03 13:46:19 -0300

    feat: adicionando validação após estiver logado

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
+import { PATHS } from "@/routes/paths";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated) {
+    return <Navigate to={PATHS.home} replace />
+  }
+
   return (
     <div style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
       <Outlet />
